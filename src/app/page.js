cf7c14fcf00029e8001c2760ea739c36bdb63fac
aayush6701 <aayuch.faltu@gmail.com>
2025-04-13 16:05:17 +0530

    gm

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, Suspense } from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import VideoPlayer from './VideoPlayer';
